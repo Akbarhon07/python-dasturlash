c038a9235616c1aa77c6d49b0d3e678287650568
Akbarhon07 <yakbarjon2@gmail.com>
2025-11-07 09:20:01 +0300

    YAngi uzgartirilgani:

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# listga harflar chiqarish
 ism = input("Ismingizni kiriting:> ")
 letters = []
 for i in ism:
@@ -6,3 +7,12 @@ print(letters)
 
 
 
+# listdagi o'xshash narsalarni o'chirish
+lst = [1, 2, 3, 3, 4, 5, 5, 6, 7]
+
+new_list = []
+
+for i in lst:
+    if i not in new_list:
+        new_list.append(i)
+print(new_list)        
